5555e0610c9e906a37d15b8548d08383a4638c0b
Øistein Sørensen <sorenso@gmail.com>
2016-06-18 23:35:04 +0200

    Added the first real test.

---
+++ test/lib/geo-lib-test.js
@@ -0,0 +1,34 @@
+/*
+ * https://github.com/5orenso
+ *
+ * Copyright (c) 2016 Øistein Sørensen
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+let buster = require('buster'),
+    assert = buster.assert,
+    path = require('path'),
+    appPath = path.normalize(__dirname + '/../../');
+
+buster.testCase('lib/my-app', {
+    setUp: () => {
+    },
+    tearDown: () => {
+        delete require.cache[require.resolve(appPath + 'lib/geo-lib')];
+    },
+    'Test module:': {
+        'exposed run function w/options': () => {
+            let myapp = require(appPath + 'lib/geo-lib');
+            let result = myapp.distance({
+                lat1: 70.3369224, lon1: 30.3411273,
+                lat2: 59.8939528, lon2: 10.6450348,
+                unit: 'km'
+            });
+            assert.equals(result, {
+                distance: 1447.35,
+                unit: 'km'
+            });
+        }
+    }
+});
