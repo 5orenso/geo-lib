53db975256f4b03cd9166c6ebd26b2a8c3bc3f2e
Øistein Sørensen <sorenso@gmail.com>
2016-06-18 23:34:43 +0200

    Added the first real test.

--- test/app-test.js
+++ test/app-test.js
@@ -15,13 +15,20 @@ buster.testCase('lib/my-app', {
     setUp: () => {
     },
     tearDown: () => {
-        delete require.cache[require.resolve(appPath + 'lib/my-app')];
+        delete require.cache[require.resolve(appPath + 'lib/geo-lib')];
     },
     'Test module:': {
         'exposed run function w/options': () => {
             let app = require(appPath + 'app/app');
-            let result = app.run();
-            assert.equals(result, 'Yo!');
+            let result = app.distance({
+                lat1: 70.3369224, lon1: 30.3411273,
+                lat2: 59.8939528, lon2: 10.6450348,
+                unit: 'km'
+            });
+            assert.equals(result, {
+                distance: 1447.35,
+                unit: 'km'
+            });
         }
 
     }
